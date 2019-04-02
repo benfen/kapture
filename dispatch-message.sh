b835095938dc25da16b19ff19470042eb8364353
Ben <benfenny@gmail.com>
2019-04-02 13:28:59 -0400

    Added scripts to dispatch and receive messages

---
+++ dispatch-message.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -ne 2 ] ; then
+  echo "\
+Usage: dispatch-message.sh <TOPIC> <DATA>\n\
+
+DATA may be either a string or a file path.
+"
+fi
+
+COMM="kubectl exec -t -i kafka-0 -- /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9093 --topic $1 2> /dev/null"
+
+if [ -f "${2}" ] ; then
+  cat $2 | $COMM
+else
+  echo $2 | $COMM
+fi
---
+++ receive-messages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -lt 1 ] ; then
+  echo "\
+Usage: receive-messages.sh <TOPIC> [FLAGS]
+
+FLAGS will be passed directly to the kafka script"
+fi
+
+ARGS="$*"
+
+kubectl exec -it kafka-2 -- /opt/kafka/bin/kafka-console-consumer.sh --bootstrap-server localhost:9093 --topic $ARGS
