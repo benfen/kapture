a5f1c2f3834d5984b64ab5bae093a0e39929122b
jay vyas <jay@apache.org>
2019-04-05 08:45:39 -0400

    installer

--- scripts/kapture.sh
+++ scripts/kapture.sh
@@ -1,8 +1,27 @@
-echo "total transactions: $_arg_total_transactions"
-echo "namespace: $_arg_namespace"
+echo "total transactions: $total_transactions"
+echo "namespace: $namespace"
 
-kubectl create ns $_arg_namespace
-kubectl create -f zk.yml  -n $_arg_namespace
-kubectl create -f kafka.yml -n $_arg_namespace
-kubectl create -f redis.yml -n $_arg_namespace
-kubectl create -f rk-conn.yml -n $_arg_namespace
+function exit_if_bad() {
+	do_exit=false
+	if [[ "$namespace" == "" ]]; then
+		do_exit="namespace missing"
+	fi
+	if [[ "$total_transactions" == "" ]]; then
+		do_exit="transactions missing"
+	fi
+
+	if [[ "$do_exit" == "false" ]] ; then
+		echo "Args valid: Commencing KAPTURE!"
+	else
+		echo "Found an error in arguments: $do_exit"
+		exit 1
+	fi
+}
+
+exit_if_bad
+
+kubectl create ns $namespace
+kubectl create -f zk.yml  -n $namespace
+kubectl create -f kafka.yml -n $namespace
+kubectl create -f redis.yml -n $namespace
+kubectl create -f rk-conn.yml -n $namespace
