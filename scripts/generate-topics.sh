dafeca57a653c84705277fb0b4c00f15580c8111
Ben <benfenny@gmail.com>
2019-04-05 16:28:19 -0400

    Fix kafka

--- scripts/generate-topics.sh
+++ scripts/generate-topics.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 COUNTER=$1
-echo $1
 
 while [ $COUNTER -gt 0 ]
 do
     COUNTER=$(( $COUNTER - 1 ))
-    /opt/kafka/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_URL --replication-factor 1 --partitions 3 --topic "Store_$COUNTER"
+    /opt/kafka/bin/kafka-topics.sh --create --zookeeper $ZOOKEEPER_URL --replication-factor 1 --partitions 3 --topic "Store_$COUNTER" --if-not-exists
 done
