a0eced47e2fd26be4180b63d7efac26baff3288b
Ben <benfenny@gmail.com>
2019-04-08 12:33:41 -0400

    Un-added changes
    See prior commit message.

--- scripts/kapture.sh
+++ scripts/kapture.sh
@@ -1,13 +1,7 @@
-echo "total transactions: $total_transactions"
-echo "namespace: $namespace"
-
 function exit_if_bad() {
 	do_exit=false
 	if [[ "$namespace" == "" ]]; then
 		do_exit="namespace missing"
-	fi
-	if [[ "$total_transactions" == "" ]]; then
-		do_exit="transactions missing"
 	fi
 
 	if [[ "$do_exit" == "false" ]] ; then
@@ -18,9 +12,28 @@ function exit_if_bad() {
 	fi
 }
 
+function tear_down() {
+	kubectl delete -f load-gen.yml -n $namespace
+	kubectl delete -f rk-conn.yml -n $namespace
+	kubectl delete -f redis.yml -n $namespace
+	kubectl delete -f kafka.yml -n $namespace
+	kubectl delete -f zk.yml -n $namespace
+
+	kubectl delete configmaps kapture-config -n $namespace
+
+	exit 0
+}
+
+# Temporary hack for testing
+if ! [ -z $1 ]; then
+	tear_down
+fi
+
 exit_if_bad
 
 kubectl create ns $namespace
+kubectl create configmap kapture-config --from-literal=STORE_COUNT="$stores" \
+	--from-literal=CUSTOMERS="$customers" --from-literal=SIMULATION="$simulation_time"
 kubectl create -f zk.yml  -n $namespace
 kubectl create -f kafka.yml -n $namespace
 kubectl create -f redis.yml -n $namespace
