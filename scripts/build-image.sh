bde3f00c4bd129e3be46ae39fe8435741f94586b
Ben <benfenny@gmail.com>
2019-04-09 09:12:28 -0400

    Added README for the scripts
    Removed alter-topic.sh and fixed some of the directory sensitivity in the build-image script.

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 (
-    cd dummy-kafka-messenger
+    BASEDIR=$(dirname $0)
+    cd "$BASEDIR/../dummy-kafka-messenger/"
     gradle build
+
+    cd ..
+    docker build . --tag benfen/dummy-kafka-messenger:latest
+
+    read -p "Push container to dockerhub?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        docker push benfen/dummy-kafka-messenger:latest
+    fi
 )
-
-docker build . --tag benfen/dummy-kafka-messenger:latest
-
-read -p "Push container to dockerhub?" -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    docker push benfen/dummy-kafka-messenger:latest
-fi
