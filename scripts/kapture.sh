c60875373cb104189ea0a20403466c05f2027222
Ben <benfenny@gmail.com>
2019-04-11 22:33:32 -0400

    Move kube config into folder

--- scripts/kapture.sh
+++ scripts/kapture.sh
@@ -1,3 +1,5 @@
+BASEDIR=$(dirname $0)
+
 function exit_if_bad() {
 	do_exit=false
 	if [[ "$namespace" == "" ]]; then
@@ -15,13 +17,13 @@ function exit_if_bad() {
 exit_if_bad
 
 function tear_down() {
-	kubectl delete -f prometheus.yml -n $namespace
-	kubectl delete -f load-gen.yml -n $namespace
-	kubectl delete -f rk-conn.yml -n $namespace
-	kubectl delete -f redis.yml -n $namespace
-	kubectl delete -f redis-master.yml -n $namespace
-	kubectl delete -f kafka.yml -n $namespace
-	kubectl delete -f zk.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/prometheus.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/load-gen.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/rk-conn.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/redis.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/redis-master.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/kafka.yml -n $namespace
+	kubectl delete -f $BASEDIR/../kube-config/zk.yml -n $namespace
 
 	kubectl delete configmaps kapture-config -n $namespace
 
@@ -36,13 +38,13 @@ fi
 kubectl create ns $namespace
 kubectl create configmap -n $namespace kapture-config --from-literal=STORE_COUNT="$stores" \
 	--from-literal=CUSTOMERS="$customers" --from-literal=SIMULATION="$simulation_time"
-kubectl create -f redis-master.yml -n $namespace
-kubectl create -f zk.yml  -n $namespace
-kubectl create -f kafka.yml -n $namespace
-kubectl create -f rk-conn.yml -n $namespace
+kubectl create -f $BASEDIR/../kube-config/redis-master.yml -n $namespace
+kubectl create -f $BASEDIR/../kube-config/zk.yml  -n $namespace
+kubectl create -f $BASEDIR/../kube-config/kafka.yml -n $namespace
+kubectl create -f $BASEDIR/../kube-config/rk-conn.yml -n $namespace
 
 if [ "on" = $deploy_prometheus ]; then  
-	kubectl create -f prometheus.yml -n $namespace
+	kubectl create -f $BASEDIR/../kube-config/prometheus.yml -n $namespace
 fi
 
 echo "Waiting for Redis master to start..."
@@ -51,7 +53,7 @@ until echo $role | grep -m 1 "role:master" ; do
 	role=$(kubectl exec redis-master -c master -- bash -c "redis-cli info 2> /dev/null | grep ^role") ; sleep 2
 done
 
-kubectl create -f redis.yml -n $namespace
+kubectl create -f $BASEDIR/../kube-config/redis.yml -n $namespace
 
 echo "Waiting for Redis slaves to start..."
 slaves=$(kubectl exec redis-master -c master -- bash -c "redis-cli info | grep ^connected_slaves")
@@ -60,4 +62,4 @@ until echo $slaves | grep -m 1 "connected_slaves:2" ; do
 	slaves=$(kubectl exec redis-master -c master -- bash -c "redis-cli info 2> /dev/null | grep ^connected_slaves") ; sleep 2
 done
 
-kubectl delete -f redis-master.yml -n $namespace
+kubectl delete -f $BASEDIR/../kube-config/redis-master.yml -n $namespace
