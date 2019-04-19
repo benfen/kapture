581ae857144fd9eae002e94749cc8c8d24669207
Ben <benfenny@gmail.com>
2019-04-19 15:24:39 -0400

    Add new mode to kafka messenger

--- dummy-kafka-messenger/src/main/kotlin/Main.kt
+++ dummy-kafka-messenger/src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 import io.prometheus.client.exporter.HTTPServer
 
-private val messengerMode: String? = System.getenv("MESSENGER_MODE")
+private val messengerMode = System.getenv("MESSENGER_MODE")
 
 fun main() {
     if (messengerMode != null && messengerMode.toLowerCase() == "listen") {
+        startProducer()
+    } else {
+        val mode = MessengerMode.valueOf(messengerMode)
         // Start the prometheus server
         val server = HTTPServer("0.0.0.0", 7001)
 
         try {
-            startConsumer()
+            startListener(mode)
         } finally {
             // The Prometheus server will keep the JAR running even if it fails, so it needs to be explicitly stopped on error
             server.stop()
         }
-
-    } else {
-        startProducer()
     }
 }
