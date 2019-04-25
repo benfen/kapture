7d1659311766ec555d425d14a0b9028035d54b18
Ben <benfenny@gmail.com>
2019-04-25 08:29:58 -0400

    Reformat output of benchmark
    Lowered the refresh rate of Kafka since it was probably overkill.

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -3,10 +3,13 @@
 # Ideally, at some point the cluster is so messed up that one of the commands fails and the script exits.
 set -e
 
+function prometheus_query() {
+    kubectl exec -it prometheus-k8s-0 -n monitoring -c prometheus -- wget -O - -q "http://localhost:9090/api/v1/query?query=$1"
+}
+
 function get_metrics() {
-    data=$(kubectl exec -it prometheus-k8s-0 -n monitoring -c prometheus -- wget -O - -q "http://localhost:9090/api/v1/query?query=sum(rate(bps_messages_produced%5B${2}%5D))")
-    data=$(echo $data | grep -o "\"value\":\[.*\"" | awk '{split($0,a,","); print a[2]}')
-    echo "Generators : $1 ; Messages/s[${2}] : $data"
+    data=$(prometheus_query sum(rate(bps_messages_produced%5B${1}%5D)))
+    echo $data | grep -o "\"value\":\[.*\"" | awk '{split($0,a,","); print a[2]}'
 }
 
 namespace=test
@@ -20,8 +23,8 @@ git clone https://github.com/carbonrelay/prometheus-recipes.git --depth 1 .
 echo "Deploying Prometheus"
 
 # Script occasionally fails to deploy one or two things on the first pass.  Do it twice.
-./prometheus-recipes.sh $namespace --no-deploy-alertmanager --no-deploy-grafana --no-deploy-kube-state-metrics --no-deploy-node-exporter --no-deploy-prometheus-adapter
-./prometheus-recipes.sh $namespace --no-deploy-alertmanager --no-deploy-grafana --no-deploy-kube-state-metrics --no-deploy-node-exporter --no-deploy-prometheus-adapter
+./prometheus-recipes.sh $namespace -npk
+./prometheus-recipes.sh $namespace -npk
 
 flags="-p"
 
@@ -29,19 +32,25 @@ if [ "on" == $redis ]; then
     flags="${flags}r"
 fi
 
-seperator="-----------------------------------"
-echo $seperator > results.txt
+header="| Generators | Network Traffic (bytes/s) | Disk Writes (bytes/s) | Messages/s [1m] | Messages/s [2m] |"
+if [ "$mode" != "fast" ]; then
+    header="$header Messages/s [3m] |"
+fi
+
+if [ "$mode" == "slow" ]; then
+    header="$header Messages/s [5m] |"
+fi
+
+echo $header > results.txt
 results="benchmark/temp/results.txt"
 # Go to the base of this repo
 cd ../..
 ./kapture.sh $namespace 1 $flags
 
 # Wait a minute for things to settle
-sleep 120
+sleep 60
 
-if [ $mode == "fast" ]; then
-    waiting_period=180
-elif [ $mode == "slow" ]; then
+if [ $mode == "slow" ]; then
     waiting_period=420
 else
     waiting_period=300
@@ -51,18 +60,16 @@ i=1
 while [ $i -le $max_generators ] || [ $max_generators -le 0 ]; do
     sleep $waiting_period
 
-    get_metrics $i "1m" >> $results
-    get_metrics $i "2m" >> $results
+    network_receive_bytes=$(prometheus_query "sum(rate(node_network_receive_bytes_total%5B3m%5D))")
+    disk_write_bytes=$(prometheus_query "sum(rate(node_disk_written_bytes_total%5B3m%5D))")
 
-    if [ "$mode" != "fast" ]; then
-        get_metrics $i "3m" >> $results
-    fi
+    row="| $i | $network_receive_bytes | $disk_write_bytes | $(get_metrics 1m) | $(get_metrics 2m) | $(get_metrics 3m) |"
 
     if [ "$mode" == "slow" ]; then
-        get_metrics $i "5m" >> $results
+        row="$row $(get_metrics 5m) |"
     fi
 
-    echo $seperator >> $results
+    echo $row >> $results
 
     i=$((i+1))
     kubectl scale Deployment data-loader -n $namespace --replicas $i 
