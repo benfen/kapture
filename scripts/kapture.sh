0adb91e3afebcfdd60376be35e976c8ac9556665
Ben <benfenny@gmail.com>
2019-04-30 10:24:00 -0400

    Suppress spurious startup errors from Redis

--- scripts/kapture.sh
+++ scripts/kapture.sh
@@ -37,24 +37,27 @@ function deploy_prometheus() {
 }
 
 function deploy_redis() {
+	# The logic here is somewhat convoluted because of how Redis clusters behave.  A cluster must start with one (and only one)
+	# master.  In order to do this, we bootstrap a master and then add the slaves to it once it has started up.  After that, we
+	# destroy the master so that we have a Redis cluster managed and replicated by Kubernetes.
 	kubectl create -f $BASEDIR/../kube-config/redis-master.yml -n $namespace
 
 	echo "Waiting for Redis master to start..."
-	role=$(kubectl exec redis-master -n $namespace -c sentinel -- bash -c "redis-cli -p 26379 sentinel master mymaster | grep ^role-reported -A 1")
+	role=$(kubectl exec redis-master -n $namespace -c sentinel -- bash -c "redis-cli -p 26379 sentinel master mymaster | grep ^role-reported -A 1" 2> /dev/null)
 	until echo $role | grep -m 1 "master" ; do
 		sleep 2
-		role=$(kubectl exec redis-master -n $namespace -c sentinel -- bash -c "redis-cli -p 26379 sentinel master mymaster | grep ^role-reported -A 1")
+		role=$(kubectl exec redis-master -n $namespace -c sentinel -- bash -c "redis-cli -p 26379 sentinel master mymaster | grep ^role-reported -A 1" 2> /dev/null)
 	done
 
 	kubectl scale rc redis -n $namespace --replicas $redis_count
 	kubectl scale rc redis-sentinel -n $namespace --replicas 3
 
 	echo "Waiting for Redis slaves to register with the master (this may take some time)..."
-	slaves=$(kubectl exec redis-master -n $namespace -c master -- bash -c "redis-cli info | grep ^connected_slaves")
+	slaves=$(kubectl exec redis-master -n $namespace -c master -- bash -c "redis-cli info | grep ^connected_slaves" 2> /dev/null)
 	until echo $slaves | grep -m 1 "connected_slaves:[^0]" ; do
 		sleep 2
 		# kubectl gives an error when the container isn't ready that really doesn't matter, so it just gets dropped on the floor.
-		slaves=$(kubectl exec redis-master -n $namespace -c master -- bash -c "redis-cli info | grep ^connected_slaves")
+		slaves=$(kubectl exec redis-master -n $namespace -c master -- bash -c "redis-cli info | grep ^connected_slaves" 2> /dev/null)
 	done
 
 	echo "Removing Redis master..."
