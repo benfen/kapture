220da86a0b7219da77c852c18580b08dd3a7601a
Ben <benfenny@gmail.com>
2019-04-30 09:38:24 -0400

    Update documentation
    Also revert load generator back to a default of 0 replicas.

--- benchmark.sh
+++ benchmark.sh
@@ -2,7 +2,7 @@
 
 # Created by argbash-init v2.8.0
 # Rearrange the order of options below according to what you would like to see in the help message.
-# ARG_OPTIONAL_SINGLE([mode],[m],[Mode to run the benchmark in (fast, normal, slow).  Slower tests will give more accurate values and a wider range of them],[normal])
+# ARG_OPTIONAL_SINGLE([mode],[m],[Mode to run the benchmark in (normal, slow).  Slower tests will give more accurate values and a wider range of them],[normal])
 # ARG_OPTIONAL_BOOLEAN([redis],[r],[Include Redis in Kapture as part of the test],[off])
 # ARG_OPTIONAL_BOOLEAN([characterize],[],[Attempts to characterize the performance of the cluster based on previously collected data.  Will run at the end after the benchmark.  Requires python to be installed on the system.],[off])
 # ARG_POSITIONAL_SINGLE([max-generators],[The maximum number of generators to run as part of this test.  If the number is less than 1, it will run until it observes a decrease in the message throughput in Kafka],[-1])
@@ -45,7 +45,7 @@ print_help()
 	printf '%s\n' "Performs a benchmark of Kapture against a cluster"
 	printf 'Usage: %s [-m|--mode <arg>] [-r|--(no-)redis] [--(no-)characterize] [-h|--help] [<max-generators>]\n' "$0"
 	printf '\t%s\n' "<max-generators>: The maximum number of generators to run as part of this test.  If the number is less than 1, it will run until it observes a decrease in the message throughput in Kafka (default: '-1')"
-	printf '\t%s\n' "-m, --mode: Mode to run the benchmark in (fast, normal, slow).  Slower tests will give more accurate values and a wider range of them (default: 'normal')"
+	printf '\t%s\n' "-m, --mode: Mode to run the benchmark in (normal, slow).  Slower tests will give more accurate values and a wider range of them (default: 'normal')"
 	printf '\t%s\n' "-r, --redis, --no-redis: Include Redis in Kapture as part of the test (off by default)"
 	printf '\t%s\n' "--characterize, --no-characterize: Attempts to characterize the performance of the cluster based on previously collected data.  Will run at the end after the benchmark.  Requires python to be installed on the system. (off by default)"
 	printf '\t%s\n' "-h, --help: Prints help"
